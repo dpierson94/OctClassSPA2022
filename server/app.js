c34370d3690ea18dda43e64fc3d824eb6b41f0c8
dpierson94 <dpierson94@gmail.com>
2022-12-20 21:23:08 -0600

    Submit Data from API to SPA and added CORS Middleware

--- server/app.js
+++ server/app.js
@@ -6,34 +6,47 @@ const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4040; // we use || to provide a default value
-
 // Initialize the Express application
 const app = express();
 
 mongoose.connect(process.env.MONGODB);
 const db = mongoose.connection
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!"));
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
 
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
+app.use(cors);
 app.use(express.json());
 app.use(logging);
 
-db.on("error", console.error.bind(console, "Connection Error:"));
-db.once(
-  "open",
-  console.log.bind(console, "Successfully opened connection to Mongo!")
-);
-// Handle the request with HTTP GET method from http://localhost:4040/status
-// app.get("/status", (request, response) => {
-//    // Create the headers for response by default 200
-//    // Create the response body
-//    // End and return the response
-//   response.send(JSON.stringify({ message: "Service healthy" }));
-// });
+// Handle the request with HTTP GET method from npm run s
+app.get("/status", (request, response) => {
+   // Create the headers for response by default 200
+   // Create the response body
+   // End and return the response
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
 
 app.get("/status", (request, response) => {
   response.status(200).json({ message: "Service healthy" });
@@ -44,6 +57,15 @@ app.get("/users/:id", (request, response) => {
   const id = request.params.id;
   // handle GET request for post with an id of "id"
   response.send(JSON.stringify({ user_id: id }));
+});
+
+app.get("/weather/:city", (request, response) => {
+  // express adds a "params" Object to requests
+  const city = request.params.city;
+  // handle GET request for post with an id of "id"
+  response.send(
+    JSON.stringify({ current: `The weather in ${city} is 73 degrees today.` })
+  );
 });
 
 app.post("/add", (request, response) => {
@@ -57,6 +79,8 @@ app.post("/add", (request, response) => {
 });
 
 app.use("/pizzas", pizzas);
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
